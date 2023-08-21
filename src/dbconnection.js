8bd0aa47c9be6889d4f1f6006f2bad7c4455570b
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-08-21 12:51:44 -0400

    postgresql added

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,18 +1,8 @@
-import { createPool } from "mysql2/promise";
-import {
-    DB_DATABASE, 
-    DB_HOST, 
-    DB_PASSWORD, 
-    DB_PORT, 
-    DB_USER
-} from './config.js'
+import pg from 'pg'
+import {DATABASE_URL} from './config.js'
 
-export const pool = createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE
+export const pool = new pg.Pool({
+    connectionString: DATABASE_URL
 })
 
 
