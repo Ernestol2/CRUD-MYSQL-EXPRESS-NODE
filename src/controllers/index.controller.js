9aa671ab1ff5ed4f5039649f7625d080693246d6
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-08-21 12:59:01 -0400

    trycatch ping route

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,10 @@
 import {pool} from '../dbconnection.js';
 
 export const ping = async (req, res) => {
-    const [result] = await pool.query('SELECT "pong"  AS result')
-    res.json(result[0])
+    try {
+        const [result] = await pool.query('SELECT "pong"  AS result')
+        res.json(result[0])
+    } catch (error) {
+        return res.status(500).json('internal server error')
+    }
 }
